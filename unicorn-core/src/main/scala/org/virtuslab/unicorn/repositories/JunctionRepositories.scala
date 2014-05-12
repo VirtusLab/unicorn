5fedc7f14a2c922602ec7d504149332fea7e1b21
Łukasz Dubiel <bambucha14@gmail.com>
2014-05-12 05:36:14 +0200

    Move function to compiled queries.
    Solve most of #9. Usage in test for this class.

--- unicorn-core/src/main/scala/org/virtuslab/unicorn/repositories/JunctionRepositories.scala
+++ unicorn-core/src/main/scala/org/virtuslab/unicorn/repositories/JunctionRepositories.scala
@@ -14,6 +14,30 @@ protected[unicorn] trait JunctionRepositories {
    */
   class JunctionRepository[First: BaseColumnType, Second: BaseColumnType, Table <: JunctionTable[First, Second]](val query: TableQuery[Table]) {
 
+    protected def findOneQueryFun(first: Column[First], second: Column[Second]) = query.filter(row => row.columns._1 === first && row.columns._2 === second)
+
+    protected val findOneQueryCompiled = Compiled(findOneQueryFun _)
+
+    protected def existsQueryFun(first: Column[First], second: Column[Second]) = findOneQueryFun(first, second).exists
+
+    protected val existsQuery = Compiled(existsQueryFun _)
+
+    protected def findByFirstFun(first: Column[First]) = query.filter(_.columns._1 === first)
+
+    protected val findByFirstQueryCompiled = Compiled(findByFirstFun _)
+
+    protected def findSecondByFirstFun(first: Column[First]) = findByFirstFun(first).map(_.columns._2)
+
+    protected val findSecondByFirstQuery = Compiled(findSecondByFirstFun _)
+
+    protected def findBySecondFun(second: Column[Second]) = query.filter(_.columns._2 === second)
+
+    protected val findBySecondQuery = Compiled(findBySecondFun _)
+
+    protected def findFirstBySecondFun(second: Column[Second]) = findBySecondFun(second).map(_.columns._1)
+
+    protected val findFirstBySecondQuery = Compiled(findFirstBySecondFun _)
+
     /**
      * Deletes one element.
      *
@@ -22,7 +46,7 @@ protected[unicorn] trait JunctionRepositories {
      * @param session implicit session
      * @return number of deleted elements (0 or 1)
      */
-    def delete(first: First, second: Second)(implicit session: Session): Int = query.filter(row => row.columns._1 === first && row.columns._2 === second).delete
+    def delete(first: First, second: Second)(implicit session: Session): Int = findOneQueryCompiled((first, second)).delete
 
     /**
      * Checks if element exists in database.
@@ -32,7 +56,7 @@ protected[unicorn] trait JunctionRepositories {
      * @param session implicit database session
      * @return true if element exists in database
      */
-    def exists(first: First, second: Second)(implicit session: Session): Boolean = query.filter(row => row.columns._1 === first && row.columns._2 === second).exists.run
+    def exists(first: First, second: Second)(implicit session: Session): Boolean = existsQuery(first, second).run
 
     /**
      * @param session implicit session param for query
@@ -54,25 +78,25 @@ protected[unicorn] trait JunctionRepositories {
      * @param a element to query by
      * @return all b values for given a
      */
-    def forA(a: First)(implicit session: Session): Seq[Second] = query.filter(_.columns._1 === a).map(_.columns._2).run
+    def forA(a: First)(implicit session: Session): Seq[Second] = findSecondByFirstQuery(a).run
 
     /**
      * @param b element to query by
      * @return all a values for given b
      */
-    def forB(b: Second)(implicit session: Session): Seq[First] = query.filter(_.columns._2 === b).map(_.columns._1).run
+    def forB(b: Second)(implicit session: Session): Seq[First] = findFirstBySecondQuery(b).run
 
     /**
      * Delete all rows with given a value.
      * @param a element to query by
      */
-    def deleteForA(a: First)(implicit session: Session): Int = query.filter(_.columns._1 === a).delete
+    def deleteForA(a: First)(implicit session: Session): Int = findByFirstQueryCompiled(a).delete
 
     /**
      * Delete all rows with given b value.
      * @param b element to query by
      */
-    def deleteForB(b: Second)(implicit session: Session): Int = query.filter(_.columns._2 === b).delete
+    def deleteForB(b: Second)(implicit session: Session): Int = findBySecondQuery(b).delete
 
   }
 
