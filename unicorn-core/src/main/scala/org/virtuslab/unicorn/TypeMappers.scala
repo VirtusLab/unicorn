c19eb44a47a6f6f057c525d9682435fa3cb1232e
Jerzy Müller <sidus.gemini+gh@gmail.com>
2015-02-06 15:34:05 +0100

    Fix link in docs

--- unicorn-core/src/main/scala/org/virtuslab/unicorn/TypeMappers.scala
+++ unicorn-core/src/main/scala/org/virtuslab/unicorn/TypeMappers.scala
@@ -14,19 +14,19 @@ trait TypeMappers {
    */
   trait CustomTypeMappers {
 
-    /** Type mapper for [[org.joda.time.DateTime]] */
+    /** Type mapper for `org.joda.time.DateTime` */
     implicit val dateTimeMapper: BaseColumnType[DateTime] = MappedColumnType.base[DateTime, Timestamp](
       dt => new Timestamp(dt.getMillis),
       ts => new DateTime(ts.getTime)
     )
 
-    /** Type mapper for [[org.joda.time.LocalDate]] */
+    /** Type mapper for `org.joda.time.LocalDate` */
     implicit val localDateMapper: BaseColumnType[LocalDate] = MappedColumnType.base[LocalDate, Date](
       dt => new Date(dt.toDate.getTime),
       d => new LocalDate(d.getTime)
     )
 
-    /** Type mapper for [[org.joda.time.Duration]] */
+    /** Type mapper for `org.joda.time.Duration` */
     implicit val durationTypeMapper: BaseColumnType[Duration] = MappedColumnType.base[Duration, Long](
       d => d.getMillis,
       l => Duration.millis(l)
