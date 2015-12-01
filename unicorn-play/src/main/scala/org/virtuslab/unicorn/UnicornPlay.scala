414c5ee547e4eecae4ac2304014dbb9139234762
pbatko <pawel.batko@gmail.com>
2015-12-01 17:11:56 +0100

    cont

--- unicorn-play/src/main/scala/org/virtuslab/unicorn/UnicornPlay.scala
+++ unicorn-play/src/main/scala/org/virtuslab/unicorn/UnicornPlay.scala
@@ -1,13 +1,17 @@
 package org.virtuslab.unicorn
 
 import org.virtuslab.unicorn.utils.Invoker
+import play.api.inject.ApplicationLifecycle
+import play.api.{ Configuration, Environment, Application }
 import play.api.data.format.Formats._
 import play.api.data.format.Formatter
-import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.{ SlickApi, DbName, SlickModule, DatabaseConfigProvider }
 import play.api.libs.json.Format
 import play.api.mvc.{ PathBindable, QueryStringBindable }
+import slick.backend.DatabaseConfig
 import slick.driver.JdbcDriver
 import slick.lifted.{ AppliedCompiledFunction, Rep }
+import slick.profile.BasicProfile
 
 import scala.language.implicitConversions
 
@@ -24,7 +28,22 @@ trait UnicornPlayLike[Underlying]
 
   def underlyingPathBinder: PathBindable[Underlying]
 
-  override lazy val driver: JdbcDriver = DatabaseConfigProvider.get(play.api.Play.current).driver
+  //  override lazy val driver: JdbcDriver = DatabaseConfigProvider.get(play.api.Play.current).driver
+  override lazy val driver: JdbcDriver = getDbDriver[JdbcDriver].driver
+
+  def getDbDriver[P <: BasicProfile](): DatabaseConfig[P] = {
+    val dbName = configuration.underlying.getString(SlickModule.DefaultDbName)
+    slickApi.dbConfig[P](DbName(dbName))
+  }
+
+  //  @throws(classOf[IllegalArgumentException])
+  //  def apply[P <: BasicProfile](implicit app: Application): DatabaseConfig[P] = {
+  //    val defaultDbName =
+  //      this (defaultDbName)
+  //  }
+  //
+  //  @throws(classOf[IllegalArgumentException])
+  //  def apply[P <: BasicProfile](dbName: String)(implicit app: Application): DatabaseConfig[P] =
 
   override type IdCompanion[Id <: BaseId] = PlayCompanion[Id]
 
@@ -37,6 +56,15 @@ trait UnicornPlayLike[Underlying]
       invokeAction(acf.result.headOption)
     }
   }
+
+  def environment: Environment
+
+  def configuration: Configuration
+
+  def applicationLifecycle: ApplicationLifecycle
+
+  def slickApi: SlickApi
+
 }
 
 class UnicornPlay[Underlying](implicit val underlyingFormatter: Formatter[Underlying],
@@ -44,6 +72,21 @@ class UnicornPlay[Underlying](implicit val underlyingFormatter: Formatter[Underl
   val underlyingQueryStringBinder: QueryStringBindable[Underlying],
   val underlyingPathBinder: PathBindable[Underlying],
   val ordering: Ordering[Underlying])
-    extends UnicornPlayLike[Underlying]
+    extends UnicornPlayLike[Underlying] {
+
+  var _environment: Environment = null
+  var _configuration: Configuration = null
+  var _applicationLifecycle: ApplicationLifecycle = null
+  var _slickApi: SlickApi = null
+
+  override def environment: Environment = _environment
+
+  override def configuration: Configuration = _configuration
+
+  override def applicationLifecycle: ApplicationLifecycle = _applicationLifecycle
+
+  override def slickApi = _slickApi
+
+}
 
 object LongUnicornPlay extends UnicornPlay[Long]
