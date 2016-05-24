6b70ef08b00a7a2b07c337a0e13e945b493eef3f
Krzysztof Borowski <kborowski@virtuslab.com>
2016-05-24 15:37:57 +0200

    remove cross build settings

--- project/Settings.scala
+++ project/Settings.scala
@@ -14,8 +14,6 @@ object Settings {
   val core = Seq(
     organization := "org.virtuslab",
     scalaVersion := "2.11.7",
-    crossScalaVersions := Seq(scalaVersion.value),
-    releaseCrossBuild := true,
     releasePublishArtifactsAction := PgpKeys.publishSigned.value,
       pomExtra := <url>https://github.com/VirtusLab/unicorn</url>
       <licenses>
