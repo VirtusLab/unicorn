c5bc402978a1a84bd937e6a2e03aa6bdf692f574
Szymon Tracz <szymon@tracz.xyz>
2023-08-01 16:05:22 +0200

    Bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,26 +3,26 @@ import sbt._
 object Dependencies {
 
   val mainCore: Seq[ModuleID] = Seq(
-    "com.typesafe.slick" %% "slick" % "3.3.3",
-    "joda-time" % "joda-time" % "2.10.10",
-    "org.joda" % "joda-convert" % "2.2.1"
+    "com.typesafe.slick" %% "slick" % "3.4.1",
+    "joda-time" % "joda-time" % "2.12.5",
+    "org.joda" % "joda-convert" % "2.2.3"
   )
 
   val testCore = Seq(
-    "org.scalatest" %% "scalatest" % "3.2.9" % "test",
-    "com.h2database" % "h2" % "1.4.200" % "test",
-    "ch.qos.logback" % "logback-classic" % "1.2.3" % "test"
+    "org.scalatest" %% "scalatest" % "3.2.16" % "test",
+    "com.h2database" % "h2" % "2.2.220" % "test",
+    "ch.qos.logback" % "logback-classic" % "1.4.8" % "test"
   )
 
   val core: Seq[ModuleID] = mainCore ++ testCore
 
   val mainPlay = Seq(
-    "com.typesafe.play" %% "play-slick" % "5.0.0"
+    "com.typesafe.play" %% "play-slick" % "5.1.0"
   )
 
   val testPlay = Seq(
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
-    "com.typesafe.play" %% "play-test" % "2.8.8" % "test"
+    "com.typesafe.play" %% "play-test" % "2.8.20" % "test"
   )
 
   val play = mainPlay ++ testPlay
