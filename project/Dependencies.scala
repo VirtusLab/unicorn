71901e95436bff6f92c3320ffe549fede220b9a5
odisseus <odisseus@users.noreply.github.com>
2025-07-11 05:41:09 +0200

    Bumped versions of Scala and dependencies.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,26 +4,26 @@ object Dependencies {
 
   val mainCore: Seq[ModuleID] = Seq(
     "com.typesafe.slick" %% "slick" % "3.4.1",
-    "joda-time" % "joda-time" % "2.12.5",
-    "org.joda" % "joda-convert" % "2.2.3"
+    "joda-time" % "joda-time" % "2.14.0",
+    "org.joda" % "joda-convert" % "3.0.1"
   )
 
-  val testCore = Seq(
-    "org.scalatest" %% "scalatest" % "3.2.16" % "test",
-    "com.h2database" % "h2" % "2.2.220" % "test",
-    "ch.qos.logback" % "logback-classic" % "1.4.8" % "test"
+  val testCore: Seq[ModuleID] = Seq(
+    "org.scalatest" %% "scalatest" % "3.2.19" % "test",
+    "com.h2database" % "h2" % "2.3.232" % "test",
+    "ch.qos.logback" % "logback-classic" % "1.5.18" % "test"
   )
 
   val core: Seq[ModuleID] = mainCore ++ testCore
 
-  val mainPlay = Seq(
+  val mainPlay: Seq[ModuleID] = Seq(
     "com.typesafe.play" %% "play-slick" % "5.1.0"
   )
 
-  val testPlay = Seq(
+  val testPlay: Seq[ModuleID] = Seq(
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
     "com.typesafe.play" %% "play-test" % "2.8.20" % "test"
   )
 
-  val play = mainPlay ++ testPlay
+  val play: Seq[ModuleID] = mainPlay ++ testPlay
 }
